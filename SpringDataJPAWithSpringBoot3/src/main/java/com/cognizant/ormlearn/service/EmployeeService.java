a08b9f33b2e5e8ca30c6c851e2deaa511dd7c69e
Punyashree M <punyahegde016@gmail.com>
2026-07-15 19:38:27 +0530

    Completed Demonstrate implementation of O/R Mapping hands-on

---
+++ SpringDataJPAWithSpringBoot3/src/main/java/com/cognizant/ormlearn/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.cognizant.ormlearn.service;
+
+import com.cognizant.ormlearn.entity.Employee;
+import com.cognizant.ormlearn.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public Employee get(int id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    @Transactional
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
