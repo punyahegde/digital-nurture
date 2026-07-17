52d942515e3ccfcf51de561210e5175bf407f66a
Punyashree M <punyahegde016@gmail.com>
2026-07-17 16:05:59 +0530

    Complete Spring REST Country Web Service hands-on

---
+++ SpringRESTusingSpringBoot4/src/main/java/com/cognizant/springrestusingspringboot2/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.cognizant.springrestusingspringboot2.controller;
+
+import com.cognizant.springrestusingspringboot2.model.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        LOGGER.info("START");
+        LOGGER.info("END");
+        return "Hello World!!";
+    }
+
+    @GetMapping("/country")
+    public Country getCountry() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        LOGGER.info("END");
+
+        return country;
+    }
+}
