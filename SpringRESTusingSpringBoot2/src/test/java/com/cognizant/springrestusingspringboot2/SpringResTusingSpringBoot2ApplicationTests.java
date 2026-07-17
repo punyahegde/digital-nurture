1a4e20bee9c826ef26fb3b126bb3d244c28021de
Punyashree M <punyahegde016@gmail.com>
2026-07-17 15:22:21 +0530

    Add Spring REST Hands-on 4 - Load Country from Spring Configuration XML

---
+++ SpringRESTusingSpringBoot2/src/test/java/com/cognizant/springrestusingspringboot2/SpringResTusingSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springrestusingspringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResTusingSpringBoot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
