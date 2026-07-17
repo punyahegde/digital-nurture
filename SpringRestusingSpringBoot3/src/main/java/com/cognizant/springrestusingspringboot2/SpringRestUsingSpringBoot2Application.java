68fa83aea6b98e09f448438abadd50762d4debf2
Punyashree M <punyahegde016@gmail.com>
2026-07-17 15:47:02 +0530

    Complete Spring REST Hello World RESTful Web Service hands-on

---
+++ SpringRestusingSpringBoot3/src/main/java/com/cognizant/springrestusingspringboot2/SpringRestUsingSpringBoot2Application.java
@@ -0,0 +1,37 @@
+package com.cognizant.springrestusingspringboot2;
+
+import com.cognizant.springrestusingspringboot2.model.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringRestUsingSpringBoot2Application {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringRestUsingSpringBoot2Application.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringRestUsingSpringBoot2Application.class, args);
+
+        displayCountry();
+    }
+
+    public static void displayCountry() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country);
+
+        LOGGER.info("END");
+    }
+}
